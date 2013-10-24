cb49640ea87fad6a35f16edfc7dc40073ccd52f2
taurheim <nikothevlan@hotmail.com>
2013-10-24 12:20:41 -0400

    Character v1
    also some initial testing about fps and ticks

--- joe-niko-2013/main.py
+++ joe-niko-2013/main.py
@@ -1,8 +1,15 @@
 '''
 @author Joe Crozier & Niko Savas
 '''
+import pygame
 
+pygame.init()
 #import shit
+
+
+running = True #If running is true, the game will play
+tick_timer = pygame.time.Clock() #This timer will cap fps and tick once every ~16ms (60fps)
+last_second = 0
 
 # Init function:
 ## make display, set clock for framerate,
@@ -11,3 +18,10 @@
 ## Check if the game is still running, otherwise close game
 ## Tick clock
 ## Check for user inputs
+
+def rungame():
+    while running:
+        tick_timer.tick(60)
+        last_second = pygame.time.get_ticks() - last_second
+        print last_second
+        
