21fb4fcac8e21ec60dc7c42d214c9446cb830f83
taurheim <nikothevlan@hotmail.com>
2013-10-24 13:10:59 -0400

    getting the Main class to work

--- joe-niko-2013/main.py
+++ joe-niko-2013/main.py
@@ -6,22 +6,42 @@ import pygame
 pygame.init()
 #import shit
 
+#GAME SETTINGS
+FPS = 60
+WINDOW_WIDTH = 800
+WINDOW_HEIGHT = 600
+
 
 running = True #If running is true, the game will play
 tick_timer = pygame.time.Clock() #This timer will cap fps and tick once every ~16ms (60fps)
-last_second = 0
 
 # Init function:
-## make display, set clock for framerate,
+## make display
 
-# Main loop:
-## Check if the game is still running, otherwise close game
-## Tick clock
-## Check for user inputs
 
-def rungame():
-    while running:
-        tick_timer.tick(60)
-        last_second = pygame.time.get_ticks() - last_second
-        print last_second
+class Main:
+    def initgame():
+        Game = pygame.display.set_mode((WINDOW_WIDTH,WINDOW_HEIGHT))
+        rungame()
+    # Main loop:
+    ## Check if the game is still running, otherwise close game
+    ## Tick clock
+    ## Check for user inputs
+    def game_loop(self):
+        self.framecount = 0 #how many frames have elapsed (resets every second)
+
         
+        while running:
+            tick_timer.tick(60)
+            self.framecount+=1
+
+            total_frames =0
+            
+            if(self.framecount==60):
+                self.framecount=0
+                total_frames+=1
+                print total_frames,"--",pygame.time.get_ticks()
+            
+#Run the game loop
+MainObject = Main()
+MainObject.game_loop()
