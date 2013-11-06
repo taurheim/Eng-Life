a0c99a96e071beb91cfb3b8474f727cb9afad1b3
taurheim <nikothevlan@hotmail.com>
2013-11-06 08:35:08 -0500

    Titlescreen 2

--- joe-niko-2013/menu.py
+++ joe-niko-2013/menu.py
@@ -15,6 +15,8 @@ class TitleScreen():
         self.back,null = gfx.load_image("titlescreen/bg.png",-1)
         self.playbtn = [gfx.load_image("titlescreen/playhover.png",-1)[0],pygame.Rect(109,460,174,72)]
         self.instbtn = [gfx.load_image("titlescreen/insthover.png",-1)[0],pygame.Rect(344,460,375,72)]
+        self.inst,rand = gfx.load_image("titlescreen/inst.png",-1)
+        self.backbtn = [gfx.load_image("titlescreen/backhover.png",-1)[0],pygame.Rect(19,514,268,72)]
     def screen_loop(self,screen):
         self.running = True
         tick_timer = pygame.time.Clock()
@@ -32,25 +34,20 @@ class TitleScreen():
                         return True
                     elif self.instbtn[1].collidepoint(mouse):
                         #Run instruction loop
-                        pass
+                        instructions = True
+                        screen.blit(self.inst, (0,0))
+                        while instructions:
+                            pygame.display.flip()
+                            mouse = pygame.mouse.get_pos()
+                            for event in pygame.event.get():
+                                if event.type == pygame.MOUSEBUTTONDOWN and event.button==1 and self.backbtn[1].collidepoint(mouse):
+                                    instructions = False
+                            if self.backbtn[1].collidepoint(mouse):
+                                screen.blit(self.backbtn[0],(19,514))
+                            else:
+                                screen.blit(self.inst, (0,0))
             mouse = pygame.mouse.get_pos()
             if self.playbtn[1].collidepoint(mouse):
                 screen.blit(self.playbtn[0],(109,460))
-                #hoverimg = self.playbtn[0].copy()
-                #hoverimg.fill((255, 255, 255, 100), None, pygame.BLEND_RGBA_MULT)
-                #print hoverimg
-                #screen.blit(hoverimg,(150,460))
             elif self.instbtn[1].collidepoint(mouse):
                 screen.blit(self.instbtn[0],(344,460))
-                
-    def fade(self,):
-        DURATION = 2.0 # seconds
-        start_time = time.clock()
-        ratio = 0.0 # alpha as a float [0.0 .. 1.0]
-        while ratio < 1.0:
-            current_time = time.clock()
-            ratio = (current_time - start_time) / DURATION
-            if ratio > 1.0: # we're a bit late
-                ratio = 1.0
-            # all your drawing details go in the following call
-            #screen.blit((self.playbtn[0][0], alpha = 255 * ratio)
